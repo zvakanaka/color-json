c34ee9ed2790663dd35bc5463925b5d41b0a1f16
Adam <8365885+zvakanaka@users.noreply.github.com>
2018-03-09 20:29:20 -0700

    v1

---
+++ tests/test.js
@@ -0,0 +1,50 @@
+const colorJson = require('../color-json.js');
+
+console.log('default usage:');
+const topPlanets = [
+  {
+    "planet": "Saturn",
+    "radius": {
+      "miles": 36184
+    },
+    "rings": true,
+    "moons": 53
+  },
+  {
+    "planet": "Earth",
+    "radius": {
+      "miles": 3959
+    },
+    "rings": null,
+    "moons": 1
+  }
+];
+
+console.log(colorJson(topPlanets));
+
+console.log('switch colors:');
+const customColors = {
+  separator: 'yellow',
+  string: 'yellow',
+  number: 'blue',
+  boolean: 'magenta',
+  null: 'red',
+  key: 'green'
+};
+console.log(colorJson(topPlanets, customColors));
+
+console.log('custom color map:');
+const customColorMap = {
+  black: '\x1b[38;2;0;0;0m',
+  red: '\x1b[38;2;249;133;123m',
+  green: '\x1b[38;2;163;238;160m',
+  yellow: '\x1b[38;2;209;154;102m',
+  blue: '\x1b[38;2;75;167;239m',
+  magenta: '\x1b[38;2;209;154;102m',
+  cyan: '\x1b[36m',
+  white: '\x1b[38;2;219;223;244m'
+};
+console.log(colorJson(topPlanets, undefined, customColorMap));
+
+console.log('custom spacing:');
+console.log(colorJson(topPlanets, undefined, undefined, 4));
