374fa285dc950e1bfc43160b1134af4c3400edfe
Adam Quinton <8365885+zvakanaka@users.noreply.github.com>
2023-01-16 12:09:46 -0700

    Apply suggestions from code review
    Js-docs and types updates.

--- color-json.d.ts
+++ color-json.d.ts
@@ -1,34 +1,34 @@
 export type ColorMap = {
-    black: string,
-    red: string,
-    red: string,
-    yellow: string,
-    blue: string,
-    magenta: string,
-    cyan: string,
-    white: string
+  black: string,
+  red: string,
+  yellow: string,
+  blue: string,
+  magenta: string,
+  cyan: string,
+  white: string
 };
 
 export type Colors = {
-    separator: keyof ColorMap,
-    string: keyof ColorMap,
-    number: keyof ColorMap,
-    boolean: keyof ColorMap,
-    null: keyof ColorMap,
-    key: keyof ColorMap,
+  separator: keyof ColorMap,
+  string: keyof ColorMap,
+  number: keyof ColorMap,
+  boolean: keyof ColorMap,
+  null: keyof ColorMap,
+  key: keyof ColorMap,
 }
 
 /**
  * Given an object, it returns its JSON representation colored using
  * ANSI escape characters.
- * @param json {any} JSON object to highlighter
- * @param colors {Colors} An map with the ANSI characters for each supported color.
- * @param colorMap {Colors} An object to configure the coloring.
- * @param spacing {number} The indentation spaces.
+ * @param {(Object | string)} json - JSON object to highlighter.
+ * @param {Colors} [colors] - A map with the ANSI characters for each supported color.
+ * @param {ColorMap} [colorMap] - An object to configure the coloring.
+ * @param {number} [spacing=2] - The indentation spaces.
+ * @returns {string} Stringified JSON colored with ANSI escape characters.
  */
 export default function colorJson(
-    json: any,
-    colors: Colors,
-    colorMap: ColorMap,
-    spacing: number
+  json: Object | string,
+  colors?: Colors,
+  colorMap?: ColorMap,
+  spacing?: number
 ): string
